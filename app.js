199787a4112dba842a4d6c1f59920a1b799a8978
hamza043 <muhamadhamza474@gmail.com>
2023-11-04 15:31:57 +0500

    Javascript assigments all done

---
+++ app.js
@@ -0,0 +1,647 @@
+/*Question 1
+
+display your Name using alert
+alert("Muhammad Hamza")*/
+
+/* Question 2
+
+display your text using line breaks to break lines
+alert("Hello world \n Pakistan zindabad")*/
+
+/* Question 3 
+
+display your text in console using alert
+  console.log("Hello world cricket");
+  alert("hello world cricket");*/
+
+  /*Question 1
+
+var userName */
+
+ /*Question 2 declare variable and assign in string
+
+ var myName = "Muhammad Hamza"*/
+
+/*Question 3 declare variable and assign & display in alert
+
+var message = "hello world"
+
+alert(message)*/
+
+/*Question 4
+
+var names = "Muhammad Hamza"
+var age = +"23"
+var education = "BCOM"
+var job = "Salesman"
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+/*Question 5
+
+/var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food)*/
+
+ /*Question 6 
+
+ let children = "with 2 kids";
+ let partnersName = " and married to Abc";
+ let location = " in England";
+ let job = "you will be a Software developer";
+ 
+ alert(job + location + partnersName + " " + children);
+
+ not working*/
+ 
+ /*Question 7
+
+ var email = "abc2@gmail.com"
+ alert("my email address is"  + email)*/
+
+/*Question 8
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book "   + book)*/
+
+/*Question 9
+
+var names = " My Name is Muhammad Hamza"
+var age = " I Am 23 years old"
+var education = " I Study In BCOM"
+var job = " I take mobile and cloud computing Module A class"
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+/*Question 10
+
+var para = document.write("Yay! I can write HTML content through Javascript")*/
+
+  /* 3 Variables for numbers*/
+
+/*Question 1 
+
+let age = alert("I am 21 years old")*/
+
+/*Question 2 
+
+let vistedTimes = alert("You have visited this site 20 times ")*/
+
+/*Question 3
+
+let birthYear = document.write("My birth year is 2000") */
+
+/* Question 4
+
+let vistorsName = "John Doe"
+let productTitle = "XYZ Clothing store"
+let quantity = "10 Tshirts"
+
+document.write(vistorsName  + " " + " " + "Ordered" + " "  +  quantity  + " " + " " + "on" + " " +  productTitle )*/
+
+/*Question 1
+
+display your Name using alert
+alert("Muhammad rafay")*/
+
+/* Question 2
+
+display your text using line breaks to break lines
+alert("Hello world \n Pakistan zindabad")*/
+
+/* Question 3 
+
+display your text in console using alert
+  console.log("Hello world cricket");
+  alert("hello world cricket");*/
+
+  /*Question 1
+
+var userName */
+
+ /*Question 2 declare variable and assign in string
+
+ var myName = "Muhammad Rafay"*/
+
+/*Question 3 declare variable and assign & display in alert
+
+var message = "hello world"
+alert(message)*/
+
+/*Question 4
+
+var names = "Muhammad rafay"
+var age = +"23"
+var education = "BCOM"
+var job = "Salesman"
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+/*Question 5
+
+/var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food)*/
+
+ /*Question 6 
+
+ let children = "with 2 kids";
+ let partnersName = " and married to Abc";
+ let location = " in England";
+ let job = "you will be a Software developer";
+ 
+ alert(job + location + partnersName + " " + children);
+ 
+ not working*/
+ 
+ /*Question 7
+
+ var email = "abc2@gmail.com"
+ alert("my email address is"  + email)*/
+
+/*Question 8
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book "   + book)*/
+
+/*Question 9
+
+var names = " My Name is Muhammad rafay"
+var age = " I Am 23 years old"
+var education = " I Study In BCOM"
+var job = " I take mobile and cloud computing Module A class"
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+/*Question 10
+
+var para = document.write("Yay! I can write HTML content through Javascript")*/
+
+  /* 3 Variables for numbers*/
+
+/*Question 1 
+
+let age = alert("I am 21 years old")*/
+
+/*Question 2 
+
+let vistedTimes = alert("You have visited this site 20 times ")*/
+
+/*Question 3
+
+let birthYear = document.write("My birth year is 2000") */
+
+/* Question 4
+
+let vistorsName = "John Doe"
+let productTitle = "XYZ Clothing store"
+let quantity = "10 Tshirts"
+
+document.write(vistorsName  + " " + " " + "Ordered" + " "  +  quantity  + " " + " " + "on" + " " +  productTitle )*/
+//4 Variables Names Legal & Illegal //
+
+/*Question 1
+
+let hello = "Hello World"
+let play = " Play Cricket"
+let wonderful = "Pakistan is wonderful country"*/
+
+/*Question 2
+
+let worldNames = "Pakistan"
+let yourAge = "20"
+let containerName = "Top footer"
+let yourHobbies = "Playing Cricket"
+let countryName = "England"
+let Names = "muhammad Ali"
+let Countryname = "france"
+let Age = "23"
+let Work = "salesman"
+let Hobbies = "Reading books"*/
+
+//Math Expression//
+
+/*Question 1
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber + secondNumber)*/
+
+/*Question 2
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber - secondNumber)
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber * secondNumber)
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber / secondNumber)
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber % secondNumber)*/
+
+//Question 3
+
+/*a , b
+
+let myVariable;
+alert("Value after variable declaration is: " + myVariable);*/
+
+/*c ,d 
+
+let number = 50
+alert("Intial value is : " + number);*/
+
+/* e , f 
+
+var number = 20;
+number = ++number;
+alert("Value after variable increment is: " + number);*/
+
+/*  g , h 
+
+var number = 7;
+number += 4;
+alert("Value after addition is: " + number);*/
+
+/*i , j 
+
+var number = 20;
+number = --number;
+alert("Value after variable decrement is: " + number);*/
+
+/*Question 4 
+
+var ticketPrice = 600; 
+var numberOfTickets = 5;
+var totalCost = ticketPrice * numberOfTickets;
+alert("Total cost to buy" + numberOfTickets + " tickets to a movie is " + totalCost + " PKR");*/
+
+/*Question 5
+
+var priceItem = 320;
+var priceItem2 = 400;
+var orderedQuantity = 10;
+var orderedQuantity2 = 5;
+var totalCostItem1 = priceItem * orderedQuantity;
+var totalCostItem2 = priceItem2 * orderedQuantity2;
+var overallTotalCost = totalCostItem1 + totalCostItem2;
+var receipt = "Receipt:\n";
+receipt += "Price of item 1 is" + priceItem + " PKR,  Quantity of item 1 is" + orderedQuantity + " "
+receipt += "Price of item 2 is" + priceItem2 + " PKR, Quantity of item 2 is " + orderedQuantity2 + " "
+receipt += "Total cost of your order is 2270" + overallTotalCost + " PKR";
+document.write(receipt);*/
+
+/*Question 6
+
+var studentName = prompt("Enter your name");
+var chemMarks = (prompt("Enter your Chemistry Marks"));
+var mathsMarks = (prompt("Enter your Maths Marks"));
+var phyMarks = (prompt("Enter your Physics Marks"));
+var totalMarksObtained = 900;
+var totalMarks = chemMarks + mathsMarks + phyMarks;
+var percentage = (totalMarks / totalMarksObtained) * 100;
+alert("Percentage: " + percentage + "%");*/
+
+/*Question 7
+
+var initialValue = 50; 
+var result = (((initialValue + 5) * 10) / 2);
+alert("Result: " + result);*/
+
+/*Question 8 
+
+var birthYear = parseInt(prompt("Enter your birth year"));
+var currentYear = prompt("enter your current year ")
+
+var age = currentYear - birthYear;
+
+alert("Your age is: " + age + " years");*/
+
+/*Question 9 
+
+let number = 7;
+let result = (((number + 5) * 10) / 2);
+
+console.log("The result is: " + result);*/
+
+/*Question 10
+
+let radius = 24
+ let pie = 3.142
+ let pieAndR = pie * radius
+ let circumference = 2 * pieAndR
+
+ let squareOfRadius = 24 * 24
+ let area = pie * squareOfRadius
+
+ document.write('<h1>' + 'The Geometrizer' + '</h1>')
+ document.write("<br>")
+ document.write()
+ document.write("<br>")
+ document.write("Radius of a circle : " + radius)
+ document.write("<br>")
+ document.write("The circumference is : " + circumference)
+ document.write("<br>")
+ document.write('The area is : ' + area)
+ document.write("<br>")*/
+
+/*Question 15
+
+var snacks = prompt("Enter snacks name")
+var currentAge = prompt("Enter your current age")
+var maximumAge = prompt("Enter your maximum age")
+var amountPerDay = prompt("Enter your Amount per day")
+var yearsRemaining = maximumAge - currentAge;
+var daysRemaining = yearsRemaining * 365;
+
+var totalConsumption = amountPerDay * daysRemaining;
+
+alert("You will need  " + totalConsumption + " " + snacks + " to last you until the ripe old age ");*/
+
+// User input and conditional statement //
+
+/*Question 1
+
+var names = prompt("Enter your name")
+alert(names)*/
+
+/*Question 2 
+
+
+        var number = prompt("Enter a number for the multiplication table")
+
+        var table = "Multiplication Table for " + number + ":\n";
+        table += number + " x 1 = " + (number * 1) + "\n";
+        table += number + " x 2 = " + (number * 2) + "\n";
+        table += number + " x 3 = " + (number * 3) + "\n";
+        table += number + " x 4 = " + (number * 4) + "\n";
+        table += number + " x 5 = " + (number * 5) + "\n";
+        table += number + " x 6 = " + (number * 6) + "\n";
+        table += number + " x 7 = " + (number * 7) + "\n";
+        table += number + " x 8 = " + (number * 8) + "\n";
+        table += number + " x 9 = " + (number * 9) + "\n";
+        table += number + " x 10 = " + (number * 10) + "\n";
+
+        alert(table);*/
+
+/* Question 3 
+
+var city = prompt("Enter city name");
+
+if (city === "karachi") {
+  alert("Welcome to the city of lights, Karachi!");
+} else {
+  alert("Sorry, you are not in the city of lights, Karachi.");
+}*/
+
+/* Question 4 
+
+var gender = prompt("Enter your gender");
+
+if (gender === "Male") {
+  alert("Good morning Sir.");
+} else {
+  alert("Good morning maam.");
+}*/
+
+/* Question 5 
+
+var trafficSignal = prompt("Road Traffic Signal");
+
+if (trafficSignal === "Red") {
+  alert("Vehicles must Stop.");
+} 
+else if (trafficSignal === "Yellow") {
+  alert("Vehicles should get ready to move.");
+} else {
+  alert("Vehicles can move now.");
+}*/
+
+/*Question 6
+
+var age = prompt("Enter your max age");
+var age1 = prompt("Enter your current age");
+if (age1 <= age) {
+  alert("you are welcome");
+} 
+ else {
+  alert("you are not welcome.");
+}*/
+
+/*Question 7 
+
+var currentFuel = +prompt("Enter your current fuel in litres");
+var remainingFuel = +prompt("Enter your Remaining fuel");
+if (currentFuel < remainingFuel ) {
+  alert("Your fuel is full");
+} 
+ else {
+  alert("Please refill the fuel in your car");
+}*/
+
+/* Question 9
+
+/*const percentage = prompt("Enter your percentage");
+
+let grade;
+let remarks;
+
+if (percentage >= 80) {
+    grade = "Grade A+";
+    remarks = "Excellent";
+} else if (percentage >= 70) {
+    grade = "Grade A";
+    remarks = "Good";
+} else if (percentage >= 60) {
+    grade = "Grade B";
+    remarks = "You need to improve";
+} else {
+    grade = "Fail";
+    remarks = "Sorry";
+}
+
+alert("Grade: " + grade + "\nRemarks: " + remarks + "\nPercentage: " + percentage);*/
+
+/*Question 11
+
+let secretNum = 8;
+let guess = Number(prompt("Enter your number"));
+
+if (secretNum === guess) {
+  alert("Bingo! Correct Answer");
+} else if (secretNum - guess >= -1 && secretNum - guess <= 1) {
+  alert("Close enough to the correct answer");
+} else {
+  alert("Sorry, try again.");
+}*/
+
+/*Question 12
+
+let number = Number(prompt("Enter a number"));
+
+if (number % 3 === 0) {
+  alert(number + " is divisible by 3.");
+} else {
+  alert(number + " is not divisible by 3.");
+}*/
+
+/*Question 13 
+
+let team1 = prompt("Enter the name of the first team");
+let team2 = prompt("Enter the name of the second team");
+let team1Scores = prompt("Enter the score for team 1 ");
+let team2Scores = prompt("Enter the score for  team 2");
+
+if (team1Scores > team2Scores) {
+  alert(team1 + " is the winner with a score of " + team1Scores);
+} else if (team2Scores > team1Scores) {
+  alert(team2 + " is the winner with a score of " + team2Scores);
+} else {
+  alert("It's a tie! Both teams have the same score.");
+}*/
+
+/*Question 14
+
+let string = prompt("Enter the name");
+let number = +prompt("Ennter the number");
+let boolean = prompt("Enter the boolean");
+
+if ( string === "string") {
+  alert("String is a string.");
+} else {
+  alert("String is not a string.");
+}
+
+if (number === "number") {
+  alert("Number is a number.");
+} else {
+  alert("Number is not a number.");
+}
+
+if ( boolean === "boolean") {
+  alert("Boolean is a boolean.");
+} else {
+  alert("Boolean is not a boolean.");
+}*/
+
+
+/*Question 15 
+
+
+let number1 = Number(prompt("Enter a number"));
+
+if (number1 % 2 === 0) {
+  alert(number1 + " is an even number.");
+} else {
+  alert(number1 + " is an odd number.");
+}*/
+
+
+/*Question 16 
+
+let weather = prompt("Enter the temperature");
+
+if (weather >= 40) {
+  alert("It is too hot outside.");
+} else if (weather >= 30) {
+  alert("The weather today is normal.");
+} else if (weather >= 20) {
+  alert("Today's weather is cool.");
+} else if (weather >= 10) {
+  alert("OMG, today's weather is so cool.");
+} else {
+  alert("It is very cold outside.");
+}*/
+
+
+/* Question 17 
+
+let num1 = +prompt("Enter first number")
+let num2 = +prompt("Enter second number")*/
+
+
+
+/*Question 18 
+
+
+let dayName = prompt("Enter your day name");
+
+if (dayName === "monday" || dayName === "tuesday" || dayName === "wednesday" || dayName === "thursday" || dayName === "friday") {
+  alert("It's a weekday.");
+} else if (dayName === "saturday") {
+  alert("It is the weekend.");
+} else if (dayName === "sunday") {
+  alert("It's a holiday yay.");
+} else {
+  alert("Invalid input. Please enter a valid day name.");
+}*/
+
+
+/*Question 19 
+
+let score = +prompt("Enter the Number")
+
+if(score > 50){
+  alert("You are passed")
+}
+else{
+  alert("Try again")
+}*/
+
+
+
+/*Question 20
+
+
+let num1 = Number(prompt("Enter the first number"));
+let num2 = Number(prompt("Enter the second number"));
+
+if (num1 > num2) {
+  alert(num1 + " is the greater number.");
+} else if (num2 > num1) {
+  alert(num2 + " is the greater number.");
+} else {
+  alert("Both numbers are equal.");
+}*/
+
+
+
+/*Question 21
+
+let number = Number(prompt("Enter a number"));
+
+if (number > 0) {
+  alert("It's a positive number.");
+} else if (number < 0) {
+  alert("It's a negative number.");
+} else {
+  alert("It's zero.");
+}*/
+
+
+
+
+/*Question 23
+
+let noun = prompt("Enter a noun")
+let number1 = prompt("Enter a number ")
+
+
+
+if(noun === )*/
